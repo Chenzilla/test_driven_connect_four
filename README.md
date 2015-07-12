9fba1c199ea059703bea186b5de67edf2e34fbbf
Alex Chen <alexschen94@gmail.com>
2015-07-12 14:20:26 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# test_driven_connect_four
+# Test driven Connect Four
+An exercise in test-driven development of a simple Connect Four game, playable from the console. Content from: http://www.theodinproject.com/ruby-programming/testing-ruby.
---
+++ spec/connect_four_spec.rb
@@ -0,0 +1,50 @@
+require_relative '../connect_four.rb'
+
+describe 'board' do
+
+  before :all do
+    @game = Board.new
+  end
+
+  describe 'piece placement' do
+    context 'Before any pieces are placed' do
+      it 'should be empty at start' do
+        expect(@game.grid).to eq [['empty', 'empty', 'empty'], ['empty', 'empty', 'empty'], ['empty', 'empty', 'empty']]
+      end
+    end
+
+    context 'After pieces are placed' do
+      it 'should include the piece at the corresponding grid coordinate' do
+        @game.place_piece(2, 0, 'O')
+        @game.place_piece(2, 1, 'O')
+        expect(@game.grid).to eq [['empty', 'empty', 'O'], ['empty', 'empty', 'O'], ['empty', 'empty', 'empty']]
+      end
+    end
+  end
+
+  describe 'game over function' do
+
+    context 'when the game is not over' do
+      it 'should not be game over' do
+        expect(@game.game_over).to be false
+      end
+
+      it 'should have ongoing game status' do
+        expect(@game.game_status).to eq 'Ongoing'
+      end
+    end
+
+    context 'when the game is over' do
+      it 'should return that the game is over' do
+        @game.place_piece(0, 0, 'X')
+        @game.place_piece(0, 1, 'X')
+        @game.place_piece(0, 2, 'X')
+        expect(@game.game_over?).to be true
+      end
+
+      it 'should not have ongoing game status' do
+        expect(@game.game_status).to_not eq 'Ongoing'
+      end
+    end
+  end
+end
