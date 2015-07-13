3c4cf879de15f7b0347a799c1e7a3ebcb3ae65d7
Alex Chen <alexschen94@gmail.com>
2015-07-12 19:47:02 -0500

    Add game_over method and correpsonding specs

--- connect_four.rb
+++ connect_four.rb
@@ -1,14 +1,16 @@
 class Board
-  attr_reader :grid
+  attr_reader :grid, :game_status
 
   def initialize
-    @grid = Array.new(6) {Array (7.times.map{'empty'})}
+    @height = 6
+    @width = 7
+    @grid = Array.new(@width) {Array (@height.times.map{'empty'})}
   end
 
   def place_piece(horizontal_coord, color)
     next_empty = pick_first_empty_slot(@grid[horizontal_coord])
     if next_empty 
-      @grid[0][next_empty] = color
+      @grid[horizontal_coord][next_empty] = color
     else
       return false
     end
@@ -20,4 +22,21 @@ class Board
     end
     return false
   end
+
+  def game_over?
+    vertical_count = 1
+    @grid.each_with_index do |column, column_index|
+      column.each_with_index do |slot, slot_index|
+        if slot_index != 0 && slot == column[slot_index - 1] && slot != 'empty'
+          vertical_count += 1
+          return true if vertical_count == 4
+        else 
+          vertical_count = 1
+        end
+      end
+    end
+    return false
+  end
 end
+
+
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -2,26 +2,58 @@ require_relative '../connect_four.rb'
 
 describe 'board' do
 
-  before :all do
+  before :each do
     @game = Board.new
   end
 
   it 'should be empty at start' do
-    expect(@game.grid).to eq [['empty']*7]*6
+    expect(@game.grid).to eq [['empty']*6]*7
   end
-  context 'the piece is the first in the column' do
-    it 'should place a piece in the first slot of the correct column' do
-      @game.place_piece(0, 'red')
-      expect(@game.grid).to eq [['red']+['empty']*6]+[['empty']*7]*5
+
+  describe 'piece behavior' do
+    context 'when the piece is the first in the column' do
+      it 'should be in the first slot of the correct column' do
+        @game.place_piece(0, 'red')
+        expect(@game.grid).to eq [['red']+['empty']*5]+[['empty']*6]*6
+      end
+    end
+
+    context 'when the piece is not the first piece in the column' do
+      it 'should be above an existing piece in the column' do
+        @game.place_piece(0, 'red')
+        @game.place_piece(0, 'black')
+        expect(@game.grid).to eq [['red', 'black']+['empty']*4]+[['empty']*6]*6
+      end
+    end
+
+    context 'when there are aready the max number of pieces in a column' do
+      it 'should be unable to be placed in the column' do
+        6.times { @game.place_piece(0, 'red') }
+        expect(@game.place_piece(0, 'red')).to be false
+      end
     end
   end
 
-  context 'the piece is not the first piece in the column' do
-    it 'should place a piece above an existing piece in the correct column' do
-      @game.place_piece(0, 'black')
-      expect(@game.grid).to eq [['red', 'black']+['empty']*5]+[['empty']*7]*5
+  describe 'game status behavior' do
+    context 'when there are three pieces in a row' do
+      it 'should not be game over' do
+        3.times { @game.place_piece(0, 'black') }
+        expect(@game.game_over?).to be false
+      end
+    end
+
+    context 'when there are four pieces in a row vertically' do
+      it 'should be game over' do
+        4.times { @game.place_piece(3, 'black') }
+        print @game.grid
+        expect(@game.game_over?).to be true
+      end
+    end
+
+    context 'when there are four pieces in a row horizontally' do
+      it 'should be game over' do
+
+      end
     end
   end
-
-  
 end
